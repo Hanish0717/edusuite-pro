bfa75397a6c3c4d40d0a4babd611e090726c4021
satya prakash <satyaprakashnagireddy@gmail.com>
2026-08-07 12:38:48 +0530

    fix(transport): export route component aliases in transport index.ts

--- src/modules/transport/index.ts
+++ src/modules/transport/index.ts
@@ -1,2 +1,8 @@
 export * from "./TransportService";
 export * from "./TransportComponents";
+export {
+  TransportModuleView as TransportVerificationView,
+  TransportModuleView as TransportFeesManagementView,
+  TransportModuleView as TransportNotificationsView,
+  TransportModuleView as TransportSettingsView,
+};
