8d27c8bc2c09f4eec17d0783b5fed0b1c67ee640
satya prakash <satyaprakashnagireddy@gmail.com>
2026-08-07 12:37:40 +0530

    fix(academics): import missing useMemo from react in AcademicsComponents.tsx

--- src/modules/academics/AcademicsComponents.tsx
+++ src/modules/academics/AcademicsComponents.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useAcademic } from "@/context/academic-context";
 import { getDashboardData, type AcademicDashboardData } from "./AcademicsDashboardService";
 import {
